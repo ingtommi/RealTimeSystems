8df7c5adfc434e41819ce2a53c8646000bd827d8
Tommaso Fava <tfava00@gmail.com>
2023-12-20 08:38:05 +0000

    temp update

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -2,13 +2,18 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
+#include "LM75B.h"
+#include "C12832.h"
 
 //DigitalOut led1(LED1);
+C12832 lcd(p5, p7, p6, p8, p11);
+LM75B sensor(p28, p27);
 Serial pc(USBTX, USBRX);
 
 //QueueHandle_t xQueue;
 
 extern void monitor(void);
+       void show_temp(void);
 
 /*-------------------------------------------------------------------------+
 | Function: my_fgets        (called from my_getline / monitor) 
@@ -31,13 +36,16 @@ char* my_fgets (char* ln, int sz, FILE* f)
 // CLOCK
 void vTaskClock(void *pvParameters)
 {
-
+  
 }
 
 // SENSORS
 void vTaskSensors(void *pvParameters)
 {
-
+  for(;;)
+  {
+    show_temp();
+  }
 }
 
 // PROCESSING
@@ -49,7 +57,8 @@ void vTaskProcessing(void *pvParameters)
 // CONSOLE
 void vTaskConsole(void *pvParameters)
 {
-  for(;;) {
+  for(;;) 
+  {
     //lValueToSend = 100;
     //xStatus = xQueueSend(xQueue, &lValueToSend, 0);
     monitor(); //does not return
@@ -58,17 +67,16 @@ void vTaskConsole(void *pvParameters)
 
 int main(void) {
 
-  // Perform any hardware setup necessary
-  //prvSetupHardware();
-
   pc.baud(115200);
 
   // --- APPLICATION TASKS CAN BE CREATED HERE ---
 
+  // Queues
   //xQueue = xQueueCreate(3, sizeof(int32_t));
   
+  // Tasks
   xTaskCreate(vTaskClock, "Clock", 2*configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-  xTaskCreate(vTaskSensors, "Sensors", 2*configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+  xTaskCreate(vTaskSensors, "Sensors", 2*configMINIMAL_STACK_SIZE, NULL, 3, NULL);
   xTaskCreate(vTaskProcessing, "Processing", 2*configMINIMAL_STACK_SIZE, NULL, 1, NULL);
   xTaskCreate(vTaskConsole, "Console", 2*configMINIMAL_STACK_SIZE, NULL, 2, NULL);
   
@@ -78,4 +86,11 @@ int main(void) {
   // Execution will only reach here if there was insufficient heap to start the scheduler
   for(;;);
   return 0;
+}
+
+void show_temp(void)
+{
+  lcd.cls();
+  lcd.locate(0,3);
+  lcd.printf("Temp = %.3f\n", (float)sensor.temp());
 }
