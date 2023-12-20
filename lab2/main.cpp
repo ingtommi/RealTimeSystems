cda0b6a5d264db636ac5a5ad392dfa4afbd112c2
tfachada <tom.fachada@gmail.com>
2023-12-20 19:01:29 +0000

    Clock and temperature working

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -4,6 +4,7 @@
 #include "queue.h"
 #include "LM75B.h"
 #include "C12832.h"
+#include "semphr.h"
 
 //DigitalOut led1(LED1);
 C12832 lcd(p5, p7, p6, p8, p11);
@@ -14,6 +15,10 @@ Serial pc(USBTX, USBRX);
 
 extern void monitor(void);
        void show_temp(void);
+
+SemaphoreHandle_t printing_mutex = xSemaphoreCreateMutex();
+
+unsigned hours = 0, minutes = 0, seconds = 0;
 
 /*-------------------------------------------------------------------------+
 | Function: my_fgets        (called from my_getline / monitor) 
@@ -36,7 +41,30 @@ char* my_fgets (char* ln, int sz, FILE* f)
 // CLOCK
 void vTaskClock(void *pvParameters)
 {
-  
+  for(;;)
+  {
+    xSemaphoreTake(printing_mutex, portMAX_DELAY);
+    lcd.locate(2,2);
+    lcd.printf("%02d:%02d:%02d", hours, minutes, seconds);
+    xSemaphoreGive(printing_mutex);
+
+    if (seconds < 59) seconds++;
+    else
+    {
+      seconds = 0;
+      if (minutes < 59)
+      {
+        minutes++;
+      }
+      else
+      {
+        minutes = 0;
+        hours++;
+      }
+    }
+
+    vTaskDelay( pdMS_TO_TICKS( 1000 ) );
+  }
 }
 
 // SENSORS
@@ -44,14 +72,24 @@ void vTaskSensors(void *pvParameters)
 {
   for(;;)
   {
-    show_temp();
+    xSemaphoreTake(printing_mutex, portMAX_DELAY);
+    lcd.locate(2,19);
+    lcd.printf("%d C ", (int)sensor.temp());
+    //lcd.locate(30, 19);
+    //lcd.printf("L %d", (int)sensor.read8(p21));
+    xSemaphoreGive(printing_mutex);
+    
+    vTaskDelay( pdMS_TO_TICKS( 3000 ) );
   }
 }
 
 // PROCESSING
 void vTaskProcessing(void *pvParameters)
 {
-
+  for(;;)
+  {
+    vTaskDelay( pdMS_TO_TICKS( 1000 ) );    //temporary
+  }
 }
 
 // CONSOLE
@@ -62,6 +100,7 @@ void vTaskConsole(void *pvParameters)
     //lValueToSend = 100;
     //xStatus = xQueueSend(xQueue, &lValueToSend, 0);
     monitor(); //does not return
+    vTaskDelay( pdMS_TO_TICKS( 1000 ) );    //temporary
   }
 }
 
@@ -75,10 +114,10 @@ int main(void) {
   //xQueue = xQueueCreate(3, sizeof(int32_t));
   
   // Tasks
-  xTaskCreate(vTaskClock, "Clock", 2*configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-  xTaskCreate(vTaskSensors, "Sensors", 2*configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+  xTaskCreate(vTaskClock, "Clock", 2*configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+  xTaskCreate(vTaskSensors, "Sensors", 2*configMINIMAL_STACK_SIZE, NULL, 2, NULL);
   xTaskCreate(vTaskProcessing, "Processing", 2*configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-  xTaskCreate(vTaskConsole, "Console", 2*configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+  xTaskCreate(vTaskConsole, "Console", 2*configMINIMAL_STACK_SIZE, NULL, 1, NULL);
   
   // Start the created tasks running
   vTaskStartScheduler();
@@ -86,11 +125,4 @@ int main(void) {
   // Execution will only reach here if there was insufficient heap to start the scheduler
   for(;;);
   return 0;
-}
-
-void show_temp(void)
-{
-  lcd.cls();
-  lcd.locate(0,3);
-  lcd.printf("Temp = %.3f\n", (float)sensor.temp());
 }
